a3fdec274bf7dc0bced3fb0bc1590230b64e8c2c
Dmitry Obukhov <dmitry.obukhov@gmail.com>
2016-06-25 12:17:23 -0700

    Initial

---
+++ NoSleep/ProcessIcon.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SystemTrayApp.Properties;
+using System.Runtime.InteropServices;
+
+namespace SystemTrayApp
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	class ProcessIcon : IDisposable
+	{
+		/// <summary>
+		/// The NotifyIcon object.
+		/// </summary>
+		NotifyIcon ni;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ProcessIcon"/> class.
+		/// </summary>
+		public ProcessIcon()
+		{
+			// Instantiate the NotifyIcon object.
+			ni = new NotifyIcon();
+		}
+
+		/// <summary>
+		/// Displays the icon in the system tray.
+		/// </summary>
+		public void Display()
+		{
+			// Put the icon in the system tray and allow it react to mouse clicks.			
+			ni.MouseClick += new MouseEventHandler(ni_MouseClick);
+			ni.Icon = Resources.SystemTrayApp;
+			ni.Text = "System Tray Utility Application Demonstration Program";
+			ni.Visible = true;
+
+			// Attach a context menu.
+			ni.ContextMenuStrip = new ContextMenus().Create();
+            SleepControl.PreventSleep();
+
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        public void Dispose()
+		{
+			// When the application closes, this will remove the icon from the system tray immediately.
+			ni.Dispose();
+            SleepControl.EnableSleep();
+
+        }
+
+		/// <summary>
+		/// Handles the MouseClick event of the ni control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.Windows.Forms.MouseEventArgs"/> instance containing the event data.</param>
+		void ni_MouseClick(object sender, MouseEventArgs e)
+		{
+			// Handle mouse button clicks.
+			if (e.Button == MouseButtons.Left)
+			{
+				// Start Windows Explorer.
+				Process.Start("explorer", null);
+			}
+		}
+	}
+
+    public static class SleepControl
+    {
+        [FlagsAttribute]
+        private enum EXECUTION_STATE : uint
+        {
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            // Legacy flag, should not be used. 
+            // ES_USER_PRESENT   = 0x00000004, 
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        public static void PreventSleep()
+        {
+            if (SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                | EXECUTION_STATE.ES_SYSTEM_REQUIRED
+                | EXECUTION_STATE.ES_AWAYMODE_REQUIRED) == 0) //Away mode for Windows >= Vista
+                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                    | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                    | EXECUTION_STATE.ES_SYSTEM_REQUIRED); //Windows < Vista, forget away mode
+        }
+
+        public static void EnableSleep()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+    }
+
+}
